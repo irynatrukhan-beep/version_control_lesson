55ee14fdce3f5c27b7cfcdfcb57395cc8f3c1e27
irynatrukhan-beep <iryna.trukhan@gmail.com>
2026-06-13 18:00:54 +0300

    changes to analytics.py, comments added

--- analytics.py
+++ analytics.py
@@ -1,4 +1,7 @@
+#import libraries
 import pandas as pd
 import numpy as np
+#create dataframe from dictionary
 df = pd.DataFrame({'Name': ['Olga', 'Maks', 'Slava'], 'Age': [22, 34, 45]})
+#print df head
 df.head()
